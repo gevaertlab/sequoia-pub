cf4be8b7658a3e172cdc0fa9f9188e86213948a7
mpizurica <pizurica.marija@gmail.com>
2024-08-13 12:04:14 -0700

    add uni in example

--- scripts/extract_resnet_features.sh
+++ scripts/extract_resnet_features.sh
@@ -4,4 +4,5 @@ python3 pre_processing/compute_resnet_features_hdf5.py \
         --ref_file ./examples/ref_file.csv \
         --patch_data_path ./examples/Patches_hdf5 \
         --feature_path ./examples/features \
-        --max_patch_number 4000
+        --max_patch_number 4000 \
+        --feat_type resnet
